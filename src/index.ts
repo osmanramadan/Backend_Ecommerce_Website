c4ef95e8eecf8aff152b85b04ae593c076db4ed2
osmanramadan <osmanramadan840@gmail.com>
2026-05-30 23:51:47 +0200

    update-in-index

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 
+
 const app: express.Application = express();
 const port = 3005;
 
@@ -13,10 +14,15 @@ const corsoptions = {
 
 app.use(cors(corsoptions));
 
+
+
+app.use('/uploads', express.static('uploads'));
+
 // this part is responsible for recieving data from frontend forms (data-form) and save it in the req.body
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
+
 
 app.use(routes);
 
